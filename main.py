cc5893e71d3aa11313875c7b95e342b2db9b699e
yheys <yheyskebede2015@gmail.com>
2025-11-07 22:36:12 +0300

    "Improve password generator with input validation"

--- main.py
+++ main.py
@@ -1,58 +1,52 @@
 import random
 print("Welcome to Password Generator!")
 lowercase_letters = [
-    'a','b','c','d','e','f','g','h','i','j','k','l','m',
-    'n','o','p','q','r','s','t','u','v','w','x','y','z'
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 ]
 uppercase_letters = [
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ]
 numbers = ['0','1','2','3','4','5','6','7','8','9']
-symbols = [
-    '!','@','#','$','%','^','&','*','(',')','_','+','-'
-    ,'|',';',':','/'
-]
-print(random.choice(symbols))
-length=int(input("Enter password length: "))
-uppercase=input("Include uppercase letters? 'yes'/'no' ").lower()
-lowercase=input("Include lowercase letters? 'yes'/'no' ").lower()
-num=input("Include numbers? 'yes'/'no' ").lower()
-symbol=input("Include symbols? 'yes'/'no' ").lower()
-password=[ ]
+
+symbols = ['!','@','#','$','%','^','&','*','(',')','_','+','-' ,'|',';',':','/']
+
+print("Your password must have alist one from 'Uppercase letters', 'lowercase letters', 'numbers' , 'symbols'!")
+try:
+    uppercase=int(input("how many Uppercase latters do you want? '1-26': "))
+    lowercase=int(input("how many Lowercase latters do you want? '1-26': "))
+    number=int(input("how many numbers  do you want? '1-10': "))
+    symbol=int(input("how many symbols  do you want? '1-17':  "))
+except ValueError:
+    print("invalid error")
+    exit()
+if uppercase==0 or lowercase==0 or number==0 or symbol==0:
+    print("Your password must have alist one from 'Uppercase letters', 'lowercase letters', 'numbers' , 'symbols'!")
+    exit()
+elif uppercase < 0 or lowercase<0 or number<0 or symbol<0:
+    print("Donot enter the negative number!")
+elif uppercase > 26 or lowercase> 26 or number> 10 or symbol>17:
+    print("This is out of the box!")
+    exit()
+else:
+    if uppercase != 0:
+        from_upper=random.choices(uppercase_letters, k=uppercase)
+
+    if lowercase != 0:
+        from_lower=random.choices(lowercase_letters, k=lowercase)
+
+    if number != 0:
+        from_number=random.choices(numbers, k=number)
+
+    if symbol != 0:
+        from_symbol = random.choices(symbols, k=symbol)
+
+    password_list=from_lower + from_upper + from_symbol + from_number
+    random.shuffle(password_list)
+    password=''.join(password_list)
+    print(password)
 
 
-while len(password)<length:
-    if uppercase=="yes":
-        upper_pass=random.choice(uppercase_letters)
-        password.append(upper_pass)
-    elif uppercase=="no":
-        pass
-    else:
-        print("Error: You must select at least one character type.")
-    if lowercase=="yes":
-        lower_pass=random.choice(lowercase_letters)
-        password.append(lower_pass)
-    elif lowercase == "no":
-        pass
-    else:
-        print("Error: You must select at least one character type.")
-    if num=="yes":
-        num_pass=random.choice(numbers)
-        password.append(num_pass)
-    elif num == "no":
-        pass
-    else:
-        print("Error: You must select at least one character type.")
-    if symbol=="yes":
-        sym_pass=random.choice(symbols)
-        password.append(sym_pass)
-    elif symbol == "no":
-        pass
-    else:
-        print("Error: You must select at least one character type.")
-random.shuffle(password)
-str_pass=""
-str_pass=str_pass.join(password)
-print(str_pass)
+
+
+
 
